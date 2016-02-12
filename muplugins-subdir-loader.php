d653320e932687fc01e7a51bd521198c95bf0e1d
Ed Gifford <egifford@blueblazeassociates.com>
2016-02-12 16:45:39 -0500

    Plugin: bumping version to 0.1.3

--- muplugins-subdir-loader.php
+++ muplugins-subdir-loader.php
@@ -3,7 +3,7 @@
 Plugin Name: MU Plugins Subdirectory Loader
 Plugin URI:  http://code.ctlt.ubc.ca
 Description: Enables the loading of plugins sitting in mu-plugins (as folders).  This plugin has to installed as a 'must-use' plugin.
-Version:     0.1.2
+Version:     0.1.3
 Author:      iamfriendly, CTLT
 Author URI:  http://ubc.ca/
 License:     GPL v2 or later
