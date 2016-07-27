124c944c9644e18e00b9f08a81ac250026bb943b
Ed Gifford <egifford@blueblazeassociates.com>
2016-07-27 16:41:51 -0400

    Adding contents of README.md to plugin file.

--- muplugins-subdir-loader.php
+++ muplugins-subdir-loader.php
@@ -17,6 +17,22 @@ Author URI:  http://ubc.ca/
 License:     GPL v2 or later
 */
 
+/*
+ * Enables the loading of plugins sitting in mu-plugins (as folders).
+ *
+ * This plugin is an adaptation of the 'subdir-loader.php' script by Richard Tape and the
+ * Centre for Teaching, Learning and Technology at the University of British Colombia.
+ *
+ * To be consistent with other Blue Blaze plugins, we've set the author information in our fork of this
+ * plugin to be Blue Blaze Associates.
+ *
+ * For more information about this plugin, read this blog post:
+ * https://richardtape.com/2014/08/22/composer-and-wordpress-mu-plugins/
+ *
+ * The original code is located in this Gist:
+ * https://gist.github.com/richardtape/05c70849e949a5017147
+ */
+
 global $CTLT_Load_MU_Plugins_In_SubDir;
 $CTLT_Load_MU_Plugins_In_SubDir = new CTLT_Load_MU_Plugins_In_SubDir();
 
