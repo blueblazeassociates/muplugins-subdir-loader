29363133030f7b68cd9fbb607b70310bb3dad96c
Ed Gifford <egifford@blueblazeassociates.com>
2016-07-28 15:23:20 -0400

    Plugin: WordPress header: adding GitHub Plugin URI.

--- muplugins-subdir-loader.php
+++ muplugins-subdir-loader.php
@@ -16,6 +16,7 @@
  * Author URI:        http://www.blueblazeassociates.com
  * License:           GPL v2 or later
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+ * GitHub Plugin URI: https://github.com/blueblazeassociates/muplugins-subdir-loader
  */
 
 /*
